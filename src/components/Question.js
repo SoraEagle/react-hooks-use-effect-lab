cf18cf9e4c699f1e8e2ec86ee85953e56db6e14a
Matthew Phillips <phillips.matthew829@gmail.com>
2022-01-26 12:59:58 -0600

    Done.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
-function Question({ question, onAnswered }) {
+function Question({ question, onAnswered }){
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    console.log("The Question component has been re-rendered");
+    setTimeOut(() => setTimeRemaining(timeRemaining - 1), 1000); //Reset timeRemaining to 10 seconds after 10 seconds have passed.
 
-  function handleAnswer(isCorrect) {
+    if(timeRemaining = 0){
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    // Is any cleanup needed? YES
+    return function cleanup(){
+      clearTimeOut();
+    }
+    // Are there any errors/warnings from using useEffect?
+  }, [timeRemaining]); //Run only when  timeRemaining changes
+
+  function handleAnswer(isCorrect){
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  const {id, prompt, answers, correctIndex} = question;
 
-  return (
+  return(
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
         const isCorrect = index === correctIndex;
-        return (
+        return(
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
